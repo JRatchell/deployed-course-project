8f88efbc90604661b3f71ab344573617394bae27
JRatchell <jrboston1@gmail.com>
2023-06-16 23:33:39 -0400

    changed callbackURL

--- models/user-model.js
+++ models/user-model.js
@@ -39,7 +39,7 @@ passport.deserializeUser(function(user, cb) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/admin"
+    callbackURL: "https://odd-pear-crocodile-tutu.cyclic.app/auth/google/admin"
 },
 function(accessToken, refreshToken, email, cb) {
     User.findOrCreate({
